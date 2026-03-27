627df1840e4e49f6a9de5c23832ab1bb5c1717d1
himanshu21saini <himanshu.saini@live.com>
2026-03-27 23:21:11 +0530

    Update route.js

--- app/api/metadata-fields/route.js
+++ app/api/metadata-fields/route.js
@@ -14,7 +14,7 @@ export async function GET(request) {
   try {
     var whereType = type ? " AND type = '" + type + "'" : ''
     var rows = await query(
-      'SELECT field_name, type, display_name, is_output FROM metadata_rows WHERE metadata_set_id = ' + parseInt(metaId) + whereType + ' ORDER BY id ASC'
+      'SELECT field_name, type, display_name,mandatory_filter_value,sample_values is_output FROM metadata_rows WHERE metadata_set_id = ' + parseInt(metaId) + whereType + ' ORDER BY id ASC'
     )
     return Response.json({ fields: rows })
   } catch (err) {
